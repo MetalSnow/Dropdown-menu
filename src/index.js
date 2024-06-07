9cb95cc71dd47a74484a33fb4a576276199881a1
MetalSnow <aymanetrend@gmail.com>
2024-06-07 21:20:54 +0800

    Complete dots navigation and add auto-advance functionality to image carousel
    - Implemented dots navigation logic for the image carousel
    - Added a timeInterval to advance slides every 5 seconds
    - Finalized styling adjustments for the carousel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import './style.css';
 import Icon from './icons/drop-down-icon.png';
 import { dropdownToggle } from './modules/dropdown.js';
-import { next, previous } from './modules/imageCarousel.js';
+import {
+  next,
+  previous,
+  swipeImage,
+  dotsColorController,
+} from './modules/imageCarousel.js';
 
 const dropdownBtn = document.querySelector('#drop-down-btn');
 const dropdownDiv = document.querySelector('.drop-down-content');
+const imgs = document.querySelectorAll('img:not(.dropdown-icon)');
+const navigationDots = document.querySelectorAll('.dot');
 
 const nextBtn = document.querySelector('#next');
 const previousBtn = document.querySelector('#previous');
@@ -20,5 +27,24 @@ dropdownBtn.addEventListener('click', () => {
   dropdownToggle(dropdownDiv);
 });
 
-nextBtn.addEventListener('click', next);
-previousBtn.addEventListener('click', previous);
+nextBtn.addEventListener('click', () => {
+  next();
+  dotsColorController(navigationDots, imgs);
+});
+previousBtn.addEventListener('click', () => {
+  previous();
+  dotsColorController(navigationDots, imgs);
+});
+
+navigationDots.forEach((dot) => {
+  dot.addEventListener('click', () => {
+    swipeImage(dot, imgs);
+    dotsColorController(navigationDots, imgs);
+  });
+});
+
+// timeOut which advances the slides every 5 seconds
+setInterval(() => {
+  next();
+  dotsColorController(navigationDots, imgs);
+}, 5000);
--- src/modules/imageCarousel.js
+++ src/modules/imageCarousel.js
@@ -1,26 +1,26 @@
-export { next, previous };
+export { next, previous, swipeImage, dotsColorController };
 
 const container = document.querySelector('.slides');
 const pictureFrame = document.querySelector('.picture-frame');
-const imgs = document.querySelectorAll('.gaming');
+const imgsDiv = document.querySelectorAll('.gaming');
 
 let imgIndex = 0;
 
 const next = () => {
   // Reset ImgIndex if it reach the maximum pictures
-  if (imgIndex === 4) {
+  if (imgIndex === 4 || imgIndex === 5) {
     imgIndex = -1;
   }
 
-  imgs.forEach((img) => {
+  imgsDiv.forEach((img) => {
     if (img.parentElement === pictureFrame) {
       container.appendChild(img);
     }
   });
 
   imgIndex += 1;
-  pictureFrame.appendChild(imgs[imgIndex]);
   console.log(imgIndex);
+  pictureFrame.appendChild(imgsDiv[imgIndex]);
 };
 
 const previous = () => {
@@ -29,14 +29,51 @@ const previous = () => {
     imgIndex = 5;
   }
 
-  imgs.forEach((img) => {
+  imgsDiv.forEach((img) => {
     if (img.parentElement === pictureFrame) {
       container.appendChild(img);
     }
   });
 
   imgIndex -= 1;
-
-  pictureFrame.appendChild(imgs[imgIndex]);
   console.log(imgIndex);
+
+  pictureFrame.appendChild(imgsDiv[imgIndex]);
+  console.log(imgIndex);
+};
+
+// Control images using dot navigation
+const swipeImage = (dot, imgs) => {
+  const dotId = dot.dataset.id;
+
+  imgsDiv.forEach((div) => {
+    imgs.forEach((img) => {
+      if ((img.parentElement === div) & (div.parentElement === pictureFrame)) {
+        container.appendChild(img.parentElement);
+      }
+
+      if ((dotId === img.dataset.id) & (img.parentElement === div)) {
+        pictureFrame.appendChild(img.parentElement);
+        imgIndex = Number(img.dataset.id);
+      }
+    });
+  });
+};
+
+const dotsColorController = (dots, imgs) => {
+  dots.forEach((dot) => {
+    // Reset all dots color
+    dot.style.backgroundColor = 'transparent';
+    imgs.forEach((img) => {
+      if (
+        pictureFrame.contains(img.parentElement) &
+        (dot.dataset.id === img.dataset.id)
+      ) {
+        console.log(pictureFrame.contains(img.parentElement));
+        console.log(dot.dataset.id);
+        console.log(img.dataset.id);
+        dot.style.backgroundColor = 'white';
+      }
+    });
+  });
 };
